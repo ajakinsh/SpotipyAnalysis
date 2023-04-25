5e3ad89a464b15c8ffe5293d2043fb049d98b95e
ajakinsh <71792857+ajakinsh@users.noreply.github.com>
2023-04-25 00:47:00 -0400

    more regex processing of lyrics b4 pres/demo

--- LyricalLexicalDiversity.py
+++ LyricalLexicalDiversity.py
@@ -33,30 +33,38 @@ def get_song_lexical_diversity(song_title, artist_name):
             blob = blob.translate(from_lang=language, to="en")
             lyrics = str(blob)
 
-        print("LYRICS BEFORE PROCESSING:\n", lyrics)
-
+        print("\nLYRICS BEFORE PROCESSING:\n", lyrics)
         lyrics = lyrics.splitlines()[1:]  # skip the first line (song and contributor info)
         lyrics = "\n".join(lyrics) # put back into string
 
         # Define a regular expression to match bracketed text that genius puts in lyrics
-        # For example the label of [Chorus] or [Pre-Chorus]
+        # For example the label of [Chorus] or (Pre-Chorus) or {Intro}
         bracketed_text_re = r'\[.*?\]'
         # Remove the bracketed text from the lyrics by replacing with empty string
         lyrics = regex.sub(bracketed_text_re, '', lyrics)
 
-        # Calculate the lexical diversity of the song
+        # Remove parentheses and curly brackets
+        parens_and_curly_brackets_re = r'[\(\)\{\}]'
+        lyrics = regex.sub(parens_and_curly_brackets_re, '', lyrics)
+        lyrics = lyrics.lower() # make all lowercase for more accurate sets
+
+        # Split the lyrics into words
         words = lyrics.split()
+        # Remove any punctuation from the beginning or end of each word
+        words = [word.strip(",.;!?") for word in words]
+        # Remove any empty words from the words list
+        words = [word for word in words if word != ""]
+
+        # Calculate the lexical diversity of the song
         unique_words = set(words)
-        print("LYRICS AFTER PROCESSING:\n", lyrics)
-
-        print("Unique words: ", unique_words)
-
+        print("\nLYRICS AFTER PROCESSING:\n", lyrics)
+        print("\nUnique words: ", sorted(unique_words))
         lexical_diversity = len(unique_words) / len(words)
 
-        print(f"Lexical diversity of {song_title} by {artist_name}:")
+        print(f"\nLexical diversity of {song_title} by {artist_name}:")
         print(f"{len(unique_words)} unique words / {len(words)} total words = {lexical_diversity}")
         return lexical_diversity
     except:
         return None
 
-get_song_lexical_diversity("Deify", "Disturbed")
+get_song_lexical_diversity("What Makes a Good Man", "The Heavy")
